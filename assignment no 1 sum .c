4860e36e7a8c0836f18e3d5d52b84cbf82fe037c
Areebaarain <areebaaicoder@gmail.com>
2024-10-19 12:53:37 +0500

    Add files via upload

---
+++ assignment no 1 sum .c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main() 
+{
+int num1; 
+int num2;
+printf("enter any 2 number/n");
+scanf("%d",& num1);
+scanf("%d",&num2) ;
+printf("the sum of %d and %d is:%d/n",num1,num2,num1+num2);
+getch() ;
+}
